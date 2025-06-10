744e93dbdd8ded1bffb6c5df08e174510262c922
Cenk Altı <cenkalti@gmail.com>
2025-06-09 20:43:16 -0400

    add various exception types

--- exception.py
+++ exception.py
@@ -4,7 +4,13 @@ from akson import Assistant, Chat
 class ExceptionRaiser(Assistant):
 
     async def run(self, chat: Chat) -> None:
-        1 / 0  # type: ignore
+        match message := chat.state.messages[-1].content.strip():
+            case "zero":
+                1 / 0  # type: ignore
+            case "assert":
+                assert False
+            case _:
+                raise Exception(message)
 
 
 exception_raiser = ExceptionRaiser()
